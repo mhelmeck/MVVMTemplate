1fce5382fdce02912f85326c11a3465196dd1236
Maciej Hełmecki <maciej.helmecki@accenture.com>
2022-10-31 01:25:46 +0100

    Create project

---
+++ MVVMTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMTemplate
+//
+//  Created by maciej.helmecki on 31/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
